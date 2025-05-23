ca36c3ecc6fc13499d0f76746e7c895687efda50
EstefanyMontiel <147006123+EstefanyMontiel@users.noreply.github.com>
2025-05-23 17:43:20 -0600

    Feat: PokemonApi Update
    Update migrations

--- PokemonApi/Program.cs
+++ PokemonApi/Program.cs
@@ -7,14 +7,14 @@ using SoapCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSoapCore();
 
-builder.Services.AddSingleton<IPokemonService, PokemonService>();
-builder.Services.AddSingleton<IPokemonRepository,PokemonRepository>();
+builder.Services.AddScoped<IPokemonService, PokemonService>();
+builder.Services.AddScoped<IPokemonRepository,PokemonRepository>();
 
-builder.Services.AddSingleton<IHobbyService, HobbyService>();
-builder.Services.AddSingleton<IHobbyRepository,HobbyRepository>();
+builder.Services.AddScoped<IHobbyService, HobbyService>();
+builder.Services.AddScoped<IHobbyRepository,HobbyRepository>();
 
-builder.Services.AddSingleton<IBookService, BookService>();
-builder.Services.AddSingleton<IBookRepository,BookRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IBookRepository,BookRepository>();
 
 builder.Services.AddDbContext<RelationalDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
 ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")))); //conexion a la base de datos
